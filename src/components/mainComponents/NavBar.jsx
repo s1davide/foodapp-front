9a5d38ae3e56de12c7c89e2fca248c068b491bb4
Sebastian David Estrada Ojeda <sdavide1010@gmail.com>
2022-01-10 17:58:06 -0500

    Changed css class and id names for general convention

--- src/components/mainComponents/NavBar.jsx
+++ src/components/mainComponents/NavBar.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
-import '../../styles/components/mainComponents/NavBar.scss'
-import Btn from '../atoms/Btn'
+import React from "react";
+import "../../styles/components/mainComponents/NavBar.scss";
+import Btn from "../atoms/Btn";
 import coolicon from "../../assets/img/coolicon.png";
 
 const NavBar = (props) => {
-    return (
-        <div id='nav-bar'>
-          {/* Left Menu Btn */}
-          <Btn ml="16px" onClick={()=>alert('epa')} curHover="pointer" img={coolicon} /> 
-        </div>
-    )
-}
-export default NavBar
+  return (
+    <div id="nav-bar">
+      {/* Left Menu Btn */}
+      <Btn
+        ml="16px"
+        onClick={() => alert("epa")}
+        curHover="pointer"
+        img={coolicon}
+      />
+    </div>
+  );
+};
+export default NavBar;
--- src/components/mainComponents/SideBar.jsx
+++ src/components/mainComponents/SideBar.jsx
@@ -3,23 +3,22 @@ import '../../styles/components/mainComponents/SideBar.scss';
 import { FaHome, FaInfoCircle, FaInstagram, FaMapMarkerAlt } from "react-icons/fa";
 
 const SideBar = () => {
-
     return (
-        <div id='side-bar'>             
-            <div className="menuItem">
-                <FaHome  className="iconMenu"/>
+        <div id='sidebar' >             
+            <div className="menu-item">
+                <FaHome  className="icon-menu"/>
                 <a href="#" >Inicio</a>
             </div>
-            <div className="menuItem">
-                <FaInfoCircle className="iconMenu"/>
+            <div className="menu-item">
+                <FaInfoCircle className="icon-menu"/>
                 <a href="#">Información</a>
             </div>
-            <div  className="menuItem">
-                <FaMapMarkerAlt  className="iconMenu"/>
+            <div  className="menu-item">
+                <FaMapMarkerAlt  className="icon-menu"/>
                 <a href="#">Ubicación</a>
             </div>
-            <div  className="menuItem">
-                <FaInstagram className="iconMenu"/>
+            <div  className="menu-item">
+                <FaInstagram className="icon-menu"/>
                 <a href="#">Instagram</a>
             </div>
             
